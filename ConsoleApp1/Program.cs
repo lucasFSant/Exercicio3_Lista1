4aee2c3e0df5154135c420907925ac7bc76f5c71
User <User@DESKTOP-TEIM933>
2024-03-02 18:05:31 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿string? n;
+int num = 0;
+double media = 0;
+
+Console.Write("Número de valores reais: ");
+n = Console.ReadLine();
+Int32.TryParse(n, out num);
+
+double[] vet = new double[num];
+
+for (int i = 0; i < vet.Length; i++)
+{
+    Console.Write("Valor do " + i + "º número: ");
+    n = Console.ReadLine();
+    Double.TryParse(n, out vet[i]);
+    media += vet[i];
+}
+
+media = media / (double)num;
+Console.WriteLine("Média aritmética: " + media);
